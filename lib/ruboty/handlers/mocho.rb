2e6f172d04e160694e31b913d6b5f854f58c6c85
mizukmb <mizukmb6@gmail.com>
2015-05-12 12:56:50 +0900

    talk meigen

--- lib/ruboty/handlers/mocho.rb
+++ lib/ruboty/handlers/mocho.rb
@@ -7,9 +7,27 @@ module Ruboty
         description: "Return はろ～(○・▽・○)",
         all: true
       )
+      on(
+        /meigen/i,
+        name: "meigen",
+        description: "talk har Witticism in random."
+      )
 
       def mocho(message)
         message.reply("はろ～(○・▽・○)")
+      end
+
+      def meigen(message)
+      meigens = [
+        "もう今さらかもですが、\n2013年はじまっちゃいましたね〜っ(ノ≧∇≦)ノわーい",
+        "みみずみたい～っ！(*^◯^*)",
+        "みんなアラーム知ってるかなー？(・ρ・*)ピーピーのやつ！",
+        '�2"v"v"v$R$7$b$ABg9%$-"2(●・▽・●)',
+      ]
+
+        random = rand(meigens.length)
+        meigen = meigens[random]
+        message.reply(meigen)
       end
     end
   end
